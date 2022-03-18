5cddab1f4d1231b5d14f81fb2bdda86d90ce24bf
lukasschulte03 <lukas007@live.se>
2022-03-18 13:59:41 +0100

    WIP

--- Game2.py
+++ Game2.py
@@ -2,12 +2,18 @@ import pygame, sys, time, random, math
 from pygame.locals import *
 pygame.init()
 pygame.display.set_caption("Drones")
-screen = pygame.display.set_mode((1920//2,1080//2))
+screen = pygame.display.set_mode((1920,1080))
 clock = pygame.time.Clock()
+
+font = pygame.font.Font(None,50)
+
+width, height = pygame.display.get_surface().get_size()
 
 circleRadius = 25
 
 liquidDensity = 1.255
+
+score = 10
 
 leftPressed = False
 rightPressed = False
@@ -17,8 +23,8 @@ biasLeft = 0
 biasRight = 0
 biasUp = 0
 
-posX = 1920//4
-posY = 1080//4
+posX = width / 2
+posY = height / 2
 velX = 0
 velY = 0
 accX = 0
@@ -43,12 +49,11 @@ while True:
     #background fill the game-window
     screen.fill((0,0,0))
 
-    #detect keystrokes and change booleans
+    #detect keystrokes and change booleans accordingly
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == K_ESCAPE):
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            print(pygame.key.name(event.key))
             if event.key == K_LEFT:
                 leftPressed = True
             if event.key == K_RIGHT:
@@ -56,7 +61,6 @@ while True:
             if event.key == K_SPACE:
                 spacePressed = True
         if event.type == pygame.KEYUP:
-            print(pygame.key.name(event.key))
             if event.key == K_LEFT:
                 leftPressed = False
             if event.key == K_RIGHT:
@@ -64,37 +68,26 @@ while True:
             if event.key == K_SPACE:
                 spacePressed = False
 
-    #set direction-biases
-    if leftPressed:
-        biasLeft = -1
-    else:
-        biasLeft = 0
-    if rightPressed:
-        biasRight = 1
-    else:
-        biasRight = 0
-    if spacePressed:
-        biasUp = -1
-    else:
-        biasUp = 0
+    #read key-booleans and set direction-biases
+    biasLeft = -1 if leftPressed else 0
+    biasRight = 1 if rightPressed else 0
+    biasUp = -1 if spacePressed else 0
     
     #reset acceleration
     accX = 0
     accY = 0
 
-    #add drag
-    drag = dragCoefficientX * ((abs(velX)*abs(velX))/2)# * math.pi * math.pow(circleRadius,2)
-    print(drag)
+    #calculate drag forces, and assign them to dragX and dragY
+    dragX = dragCoefficientX * ((abs(velX)*abs(velX))/2)
     if velX != 0:
-        accX -= (velX/abs(velX)) * drag 
-    drag = dragCoefficientY * ((abs(velY)*abs(velY))/2)# * math.pi * math.pow(circleRadius,2)
-    print(drag)
+        dragX = math.copysign(1,velX) * dragX 
+    dragY = dragCoefficientY * ((abs(velY)*abs(velY))/2)
     if velY != 0:
-        accY -= (velY/abs(velY)) * drag 
+        dragY = math.copysign(1,velY) * dragY 
 
-    #calculate acceleration to position
-    accX += (biasLeft + biasRight)*accHorizontalForce
-    accY += (biasUp * accVerticalForce) - gravityForce
+    #calculate acceleration to position (inverse derivative)
+    accX += ((biasLeft + biasRight) * accHorizontalForce) - dragX
+    accY += (biasUp * accVerticalForce) - gravityForce - dragY
     velX += accX
     velY += accY
     posX += velX
@@ -103,6 +96,11 @@ while True:
     #draw circle at posX, posY with green color
     pygame.draw.circle(screen, (0, 255, 0),
                    [posX-(circleRadius/2), posY-(circleRadius/2)], circleRadius, 0)
+
+    #draw score on top-center of screen
+    text = font.render("Score: " + str(score), True, (255,255,255))
+    text_rect = text.get_rect(center=(width/2, 25))
+    screen.blit(text, text_rect)
 
     #update the display
     pygame.display.update()
