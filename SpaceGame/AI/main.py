d0194444580305f596823390dd76941d8bcdaa32
lukasschulte03 <lukas007@live.se>
2022-03-24 22:10:02 +0100

    WIP

--- SpaceGame/AI/main.py
+++ SpaceGame/AI/main.py
@@ -1,7 +1,7 @@
+from turtle import pos
 import pygame, sys, time, random, math, neat, os
 from pygame.locals import *
 
-from Dinorun.main import distance
 pygame.init()
 pygame.display.set_caption("Drones")
 SCREEN_WIDTH = 1100
@@ -14,17 +14,46 @@ fireImg = pygame.transform.scale(pygame.image.load("images/Fire.png").convert_al
 spaceshipImg = pygame.transform.scale(pygame.image.load("images/Spaceship.png").convert_alpha(), [55,55]) 
 pointImg = pygame.transform.scale(pygame.image.load("images/Spaceship.png").convert_alpha(), [55,55]) 
 
+FONT = pygame.font.Font(None, 20)
+
 dragCoefficientX = 0.0075
 dragCoefficientY = 0.003
 gravityForce = -0.2/2
-accVerticalForce = 0.5/2
+accVerticalForce = 0.5/4
 accHorizontalForce = 0.02/2
+fps = 150
+BestFitness = 0
 
-randomPositionList = []
-for i in range(0,1000):
-    n = random.randint(10, SCREEN_HEIGHT - 10)
-    randomPositionList.append(n)
+def GenerateRandomPositionList():
+    randomPositionList = []
+    for i in range(0,1000):
+        n = random.randint(10, SCREEN_HEIGHT - 10)
+        randomPositionList.append(n)
+    return randomPositionList
 
+posList = GenerateRandomPositionList()
+
+def statistics():
+        global Spaceships, BestFitness
+
+        for i, genome in enumerate(ge):
+            if genome.fitness != 0:
+                print(genome.fitness)
+            if genome.fitness > BestFitness:
+                BestFitness = genome.fitness
+
+        text_1 = FONT.render(f'Spaceships Alive:  {str(len(Spaceships))}', True, (0, 0, 0))
+        text_2 = FONT.render(f'Generation:  {pop.generation+1}', True, (0, 0, 0))
+        text_4 = FONT.render(f'Best Fitness:  {str(BestFitness)}', True, (0, 0, 0))
+
+        SCREEN.blit(text_1, (50, 450))
+        SCREEN.blit(text_2, (50, 480))
+        SCREEN.blit(text_4, (50, 510))
+
+def distance(pos_a, pos_b):
+    dx = pos_a[0] - pos_b[0]
+    dy = pos_a[1] - pos_b[1]
+    return math.sqrt(dx**2 + dy**2)
 
 def blitRotateCenter(surf, image, angle, pos):
     rotated_image = pygame.transform.rotate(image, angle)
@@ -82,23 +111,28 @@ class Spaceship:
         blitRotateCenter(SCREEN, self.image, 0, [self.rect.x, self.rect.y])
 
 class Point:
-    def __init__(self, img = pointImg, randPosList = randomPositionList):
+    def __init__(self, img = pointImg, randPosList = posList):
         self.posIndex = 0
         self.randPosList = randPosList
         self.image = img
         self.rect = pygame.Rect(self.randPosList[1], self.randPosList[2], img.get_width(), img.get_height())
+        self.color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+        self.pointsTaken = 0
 
     def updatePos(self):
         self.posIndex += 1
+        self.pointsTaken += 1
         self.rect.x = self.randPosList[self.posIndex]
         self.rect.y = self.randPosList[self.posIndex + 1]
         
     def draw(self, SCREEN):
-        SCREEN.blit(self.image, self.rect)
+        pygame.draw.circle(SCREEN, self.color, [self.rect.x, self.rect.y], 20)
 
 
 def eval_genomes(genomes, config):
-    global Points, Spaceships, ge, nets
+    global Points, Spaceships, ge, nets, fps
+    timer = 0
+    timeSinceStart = time.time()
     clock = pygame.time.Clock()
 
     Points = []
@@ -107,16 +141,20 @@ def eval_genomes(genomes, config):
     nets = []
     #Points.append(Point())
 
+    for point in Points:
+        point.randPosList = GenerateRandomPositionList()
+
     for genome_id, genome in genomes:
         Spaceships.append(Spaceship())
+        Points.append(Point())
         ge.append(genome)
         net = neat.nn.FeedForwardNetwork.create(genome, config)
         nets.append(net)
         genome.fitness = 0
 
     run = True
-    fps = 30
     while run:
+        MousePos = pygame.mouse.get_pos()
         clock.tick(fps)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -127,11 +165,13 @@ def eval_genomes(genomes, config):
                     fps = 150
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_p:
-                    fps = 30
+                    fps = 6
     
+        timer += 1
+
         SCREEN.fill((255,255,255))
 
-        if len(Spaceships) == 0:
+        if len(Spaceships) == 0 or time.time() - timeSinceStart > 30:
             break
 
         for spaceship in Spaceships:
@@ -142,23 +182,34 @@ def eval_genomes(genomes, config):
 
 
         for i, spaceship in enumerate(Spaceships):
+            pygame.draw.line(SCREEN, Points[i].color, (Points[i].rect.x, Points[i].rect.y), (Spaceships[i].rect.x, Spaceships[i].rect.y), 3)
+            if distance(Points[i].rect, Spaceships[i].rect) < 50:
+                Points[i].updatePos()
+                ge[i].fitness += Points[i].pointsTaken
             inBounds = pygame.Rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT).collidepoint(spaceship.rect.x, spaceship.rect.y)
-            if inBounds:
-                ge[i].fitness += math.dist(spaceship.rect.x, spaceship.rect.y, SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-            else:
-                del Spaceships[i]
+            if inBounds != True:
+                Spaceships.pop(i)
+                ge.pop(i)
+                Points.pop(i)
 
 
-        for i, spaceship in enumerate(Spaceships):
-            #inputs
-            inputs = [spaceship.rect.x, spaceship.rect.y, spaceship.velX, spaceship.velY]
+
+        for i, spaceship in enumerate(Spaceships):            
+            inputs = [
+                #spaceship.rect.x, spaceship.rect.y, 
+                Spaceships[i].velX, Spaceships[i].velY, 
+                Points[i].rect.x - Spaceships[i].rect.x, Points[i].rect.y - Spaceships[i].rect.y,
+                Spaceships[i].rect.x, Spaceships[i].rect.y]
+            
+            #Store outputs
             output = nets[i].activate(inputs)
 
             #outputs
-            spaceship.leftPressed = True if output[0] < 0.5 else False
-            spaceship.rightPressed = True if output[1] < 0.5 else False
-            spaceship.upPressed = True if output[2] < 0.5 else False
+            Spaceships[i].leftPressed = True if output[0] < 0.5 else False
+            Spaceships[i].rightPressed = True if output[1] < 0.5 else False
+            Spaceships[i].upPressed = True if output[2] < 0.5 else False
 
+        statistics()
         pygame.display.update()
 
 def run(config_path):
@@ -171,7 +222,8 @@ def run(config_path):
         config_path
     )
     pop = neat.Population(config)
-    pop.run(eval_genomes, 500)
+    pop.run(eval_genomes, n=None)
+    
 
 if __name__ == '__main__':
     local_dir = os.path.dirname(__file__)
